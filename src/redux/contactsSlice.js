ae88506c4bb4cab5d0717a5bf0b348b445ef30e9
seheichenko-anna <seheichenko.anna@gmail.com>
2024-02-29 18:49:45 +0200

    Fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -7,7 +7,6 @@ const initialState = {
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ],
-  filter: '',
 };
 
 const slice = createSlice({
